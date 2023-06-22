d1dea81ae2e1991cbaf81f3e97527853408d91b8
Ugochidev <ugochukwuchioma16@gmail.com>
2023-06-22 09:18:52 +0100

    post service

---
+++ src/modules/postResources/controllers/Post.controllers.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import PostService from "../services/PostService.services";
+
+
+class PostController{
+
+}
+
+
+
+
+
+export default PostController
