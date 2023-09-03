997e5412073a79dd5463fc9d6c8506dbaf25aa41
Ugochidev <ugochukwuchioma16@gmail.com>
2023-09-03 11:34:26 +0100

    Post Controller

--- src/modules/postResources/controllers/Post.controllers.ts
+++ src/modules/postResources/controllers/Post.controllers.ts
@@ -1,13 +1,35 @@
 import { Request, Response, NextFunction } from "express";
 import PostService from "../services/PostService.services";
 
+class PostController {
+  async create(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response> {
+    try {
+      const { _id, user_id, title, body, attachments } = req.body;
+      const postService = new PostService()
+      const createPost = await postService.execute(
+ {       _id,
+        user_id,
+        title,
+        body,
+        attachments}
+      );
 
-class PostController{
-
+      return res.status(201).json({
+        success: true,
+        message: "Post created sucessfully",
+        createPost
+      });
+    } catch (error: any) {
+      return res.status(error.statusCode || 500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
 }
 
-
-
-
-
-export default PostController
+export default PostController;
